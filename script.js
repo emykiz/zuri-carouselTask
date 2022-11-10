1eedec6f44dffb33907d372bfaf610d9f722543e
Gideon Destiny Effiong <80515495+emykiz@users.noreply.github.com>
2022-11-10 20:04:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel-item');
+const totalSlides = slides.length;
+
+document.getElementById('next').addEventListener('click', function () {
+  moveToNextSlide();
+});
+document.getElementById('prev').addEventListener('click', function () {
+  moveToPrevSlide();
+});
+
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove('carousel-visible');
+    slide.classList.add('carousel-hidden');
+  }
+
+  slides[slidePosition].classList.add('carousel-visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+}
